b48c51c1b28401ae1ebfa3826b9893318c359994
Jay Gervais <mrjaygervais@gmail.com>
2019-08-15 08:14:05 -0600

    added angular packages

--- BrewApp.API/Startup.cs
+++ BrewApp.API/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BrewApp.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 
 namespace BrewApp.API
 {
@@ -25,6 +27,14 @@ namespace BrewApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<DataContext>( 
+                options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection") // replace with your Connection String
+                    // mySqlOptions =>
+                    // {
+                    //     mySqlOptions.ServerVersion(ServerType.MySql); // replace with your Server Version and Type
+                    // }
+            ));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
