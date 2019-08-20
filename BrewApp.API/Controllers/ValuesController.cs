3b798d9ef39142ddd09955bfbc043ff5e39cb884
Jay Gervais <mrjaygervais@gmail.com>
2019-08-20 16:27:44 -0600

    added registration Angular front-end

--- BrewApp.API/Controllers/ValuesController.cs
+++ BrewApp.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace BrewApp.API.Controllers
             _context = context;
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
