df8d507d10969422a109345aeabc228ef2bdbd9d
Jay Gervais <mrjaygervais@gmail.com>
2019-08-23 21:44:16 -0600

    working on member cards

--- BrewApp-SPA/src/app/routes.ts
+++ BrewApp-SPA/src/app/routes.ts
@@ -4,11 +4,20 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 export const appRoutes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'members', component: MemberListComponent },
-    { path: 'messages', component: MessagesComponent },
-    { path: 'lists', component: ListsComponent },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent },
+            { path: 'members/:id', component: MemberDetailComponent },
+            { path: 'messages', component: MessagesComponent },
+            { path: 'lists', component: ListsComponent },
+        ],
+    },
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
