7712bd0a66caafd65eb39aa625fb2dd23997ffe3
Jay Gervais <mrjaygervais@gmail.com>
2019-08-25 21:40:00 -0600

    changed detail resolver

--- BrewApp-SPA/src/app/_resolvers/member-detail.resolver.ts
+++ BrewApp-SPA/src/app/_resolvers/member-detail.resolver.ts
@@ -11,7 +11,8 @@ export class MemberDetailResolver implements Resolve<User> {
     constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User> {
-        return this.userService.getUser(route.params.id).pipe(
+        // tslint:disable-next-line: no-string-literal
+        return this.userService.getUser(route.params['id']).pipe(
             catchError(error => {
                 this.alertify.error('Problem retrieving data');
                 this.router.navigate(['/members']);
